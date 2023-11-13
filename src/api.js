90bc04d4c7cf056f391e5221def23753586f24b5
vimuths123 <vimuths@yahoo.com>
2023-11-13 17:32:54 +0530

    New changes

--- src/api.js
+++ src/api.js
@@ -41,8 +41,8 @@ app.use(`/.netlify/functions/api`, router);
 module.exports = app;
 module.exports.handler = serverless(app);
 
-const PORT = process.env.APPPORT || 9000
+// const PORT = process.env.APPPORT || 9000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`)
-}) 
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`)
+// }) 
