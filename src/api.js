7dcddadf46f89d30560bb0d75512f5a945e3316e
vimuths123 <vimuths@yahoo.com>
2023-11-13 15:59:27 +0530

    New changes

--- src/api.js
+++ src/api.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const serverless = require("serverless-http");
 
+require('dotenv').config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
+app.use(express.json());
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({
     hello: "hi!"
+  });
+});
+
+const calculateOrderAmount = (items) => {
+
+  return 1000;
+};
+
+const amount = 1000
+
+router.post("/create-payment-intent", async (req, res) => {
+  const { qty } = req.body;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount * qty,
+    currency: "usd",
+    payment_method_types: ['us_bank_account']
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
   });
 });
 
